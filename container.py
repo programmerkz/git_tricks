3e7cea837979c7c5780a15c33afee1b56b075613
Alexander Volosatov <sanya_666@mail.ru>
2020-05-04 16:02:59 +0600

    added starting container

--- container.py
+++ container.py
@@ -4,3 +4,8 @@ class Conainer:
     def __init__(self):
         self.info = "container"
         self.grpc = Grpc()
+
+
+if __name__ == '__main__':
+    c = Container()
+    print('Ok. Container created.')
